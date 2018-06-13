cf83a0e7fdc1aadbe8dfa644204fec54e1ff3b72
习惯五格 <33884004+xiguan-wuge@users.noreply.github.com>
2018-06-13 09:45:05 +0800

    手写一个new

--- index.js
+++ index.js
@@ -1,90 +1,47 @@
-// 小明 表白的故事
-// 小明 talk in 
-// js  具有强大的表现力
-// js是弱类型语言 Java 是静态强类型语言
-// js  基础类型  字符串 数字, 对象{},array,function''
-// symbol
-//  bool布尔值, false null为空 undefined
-// 六大基础类型 一个复杂类型
-
-var xiaoming={
-  name:'小明',
-  age:21,
-  hasGirlFriend:false,
-  job:null,
-  city:'Shenzheng',
-  sendFlower:function(target){
-    // 方法 行为
-    // 1 买花
-    var flower=new Flower('玫瑰');
-    if(typeof target.receiverFlower=='function')
-      target.receiverFlower(flower,this.name);
-    else
-      console.log('不想谈。。')
-
-
-    
-  }
+function Otaku(name,age){
+  this.name=name;
+  this.age=age;
+  this.habit='Games';
 }
-var xueba={
-  
+//自定义属性
+Otaku.prototype.strength=60;
+Otaku.prototype.sayYourName=function(){
+  console.log("I am "+this.name);
 }
-var xiaomei={
-  name:'小美',
-  room:'1栋',
-  hometown:'赣州',
-  age:20,
-  hasBoyFriend:false,
-  job:null,
-  city:undefined,
-  mood:'good',
-  receiverFlower:function(flower,name){
-   
-    console.log('收到'+name+'送的'+flower.type)
-  },
-  // 监听小美的好心情
-  /*
-  功能：提供心情监听
-  参数：fn 类型函数
-  在一段时间后，心情好了，将fn执行
-  */
-  ListenGoodMood:function(fn){
-    setTimeout(function(){
-    fn();
-    },1000); 
-  } 
+// 数组中最基本的对象 object
+
+// new 关键字
+// 1 要返回一个新的对象
+// 2 访问到Otaku 具有构造函数的属性
+// 3 访问到Otaku.prototype中的属性或方法
+
+// const person=new Otaku('曾凯',18);
+// person.sayYourName();
+
+
+// 参数1是类，后几个参数...
+// (js的参数不需要确定)\
+// js 的函数参数异常灵活，数量可以不定，形参可以不给，还有... reset 运算符整合起来，都会跟arguments结合起来
+function objectFactory(){
+  // var Constructor=Array.from(arguments)
+  // []代表Array
+   var Constructor=[].shift.call(arguments);
+  // console.log(Constructor,arguments)
+  var obj=new Object();
+  // 1.obj __proto__
+  // 2 构造函数上的this属性，参数要传进去，要赋值，this要指向当前的对象
+  obj.__proto__=Constructor.prototype;
+  // 在apply内部手动指定函数执行是的this
+  Constructor.apply(obj,arguments)
+  return obj;
+  // console.log(obj)
+
+// 类（构造函数，原型链对象）
+// 对象（属性或方法）
+  // console.log(arguments)
+  // var constructor=arguments[0]
+  // console.log(constructor)
+  // var args=?参数
 }
-var xiaoxue={
-  name:'小雪',
-  age:'20',
-  room:'1栋',
-  receiverFlower:function(flower,name){
-     // 实现了同样的收花方法，接口interface
-    // 小美和小雪都实现了相同的方法，实现了相同的接口
-    // 实现了相同的接口，就可以互换对象
-
-
-    // if(name=='小明'){
-    //   console.log('让我们在一起吧')
-    // }else{
-      // xiaomei .receiverFlower(flower,name);
-    // }
-    xiaomei.ListenGoodMood(function(){
-      xiaomei .receiverFlower(flower,name);
-    })
-
-    // if(xiaomei.mood=='good'){
-    //   xiaomei .receiverFlower(flower,name);
-    //   console.log('收到'+name+'送的'+flower.type)
-    // }
-    
-  }
-}
-
-// 类大写，对象小写
-var Flower=function(type){
-  this.type=type || '玫瑰'
-}
-
-// xiaoming.sendFlower(xiaomei);
-xiaoming.sendFlower(xiaoxue);
+var person=objectFactory(Otaku,'鸠摩智',50);
+person.sayYourName();
