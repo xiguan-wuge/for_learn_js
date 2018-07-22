0b889a6c0daa10b290519f51573fc15bb8775a8e
xiguan-wuge <1584077483@qq.com>
2018-07-22 23:46:05 +0800

    fs之 文件一边读并写入另一个文件

--- fs/3.js
+++ fs/3.js
@@ -6,3 +6,11 @@ const gzip = zlib.createGzip();
 const inFile = fs.createReadStream('./a.txt');
 const outFile = fs.createWriteStream('./a.txt.gz');
 inFile.pipe(gzip).pipe(outFile);   //pipe 
+
+
+// const fs = require('fs');
+// const zlib = require('zlib');
+// const gzip = zlib.createGzip();
+// const inFile = fs.createReadStream('./a.txt');
+// const outFile = fs.createWriteStream('./a.txt.gz');
+// inFile.pipe(gzip).pipe(outFile);
